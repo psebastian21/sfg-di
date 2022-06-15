16eea7f55bd1b226f96a156a63456c504736adf8
Paulo Garcia <psebastian21@gmail.com>
2022-06-14 22:44:42 -0500

    profiling

--- src/main/java/com/psebastian21/sfgdi/service/I18nSpanishGreetingService.java
+++ src/main/java/com/psebastian21/sfgdi/service/I18nSpanishGreetingService.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service("i18nService")
-@Profile("ES")
+@Profile({"ES", "default"})
 public class I18nSpanishGreetingService implements GreetingService {
 
 	@Override
